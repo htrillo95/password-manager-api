ab5455662bd46ee977e0543d77f271da062ab34a
Hector Trillo <htrillo95@gmail.com>
2025-03-23 14:23:56 -0400

    Changed individual connection variables in postgres_db.py

--- utils/postgres_db.py
+++ utils/postgres_db.py
@@ -4,7 +4,11 @@ from psycopg2.extras import RealDictCursor
 
 def get_db_connection():
     conn = psycopg2.connect(
-        os.environ['DATABASE_URL'],
+        dbname=os.environ['PGDATABASE'],
+        user=os.environ['PGUSER'],
+        password=os.environ['PGPASSWORD'],
+        host=os.environ['PGHOST'],
+        port=os.environ['PGPORT'],
         cursor_factory=RealDictCursor
     )
     return conn

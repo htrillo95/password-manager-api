0e0b5ad4ee9ec2d5fb080d05ec7c06ba53d6025c
Hector Trillo <htrillo95@gmail.com>
2025-03-31 13:42:07 -0400

    Test: Run Flask without Gunicorn using port 8080

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return '✅ Flask app is running on Railway!'
+    return "✅ Flask app running directly without Gunicorn"
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
 
 # from flask import Flask, request, jsonify
